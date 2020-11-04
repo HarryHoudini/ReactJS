f92b742887fcaa6b7d1faa8e1717c8e7c8d49897
HarryHoudini <dev.jon.jonson@gmail.com>
2020-11-04 15:43:34 +0300

    added react element and commponent

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello</h1>;
-ReactDOM.render(el, document.getElementById('root'));
+const ToDoList = () => {
+    const items = ['Learn react', 'Build Awesome App'];
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1].toUpperCase()}</li>
+        </ul>
+    );
+};
+const ToDoList2 = () => {
+    return (
+        <ul>
+            <li>Learn</li>
+            <li>Buil</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>My ToDo List </h1>;
+};
+
+const SearchPanel = () => {
+    return <input type="text" placeholder="search" />;
+};
+
+const App = () => {
+    const login = <span>Login please</span>;
+    return (
+        <div>
+            {login}
+            <span>{new Date().toString()}</span>
+            <AppHeader />
+            <SearchPanel />
+            <ToDoList />
+            <ToDoList2 />
+            <ToDoList2 />
+            <ToDoList />
+        </div>
+    );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
