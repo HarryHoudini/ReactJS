3b87aa1405c822763d4622f739a164d11b127998
HarryHoudini <dev.jon.jonson@gmail.com>
2020-12-06 18:47:03 +0300

    Disabled react/prop-types eslint rule

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
-import TodoForm from './TodoForm';
+import { TodoForm } from './TodoForm';
 
 export type ItemPops = {
     label: string;
     important: boolean;
     id: number;
 };
-
 const App: React.FC = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addHandler = (title: string) => {
+        console.log('Add New Todo', title);
+    };
     return (
         <div>
             <Navbar />
             <div className="container mt2">
-                <TodoForm />
+                <TodoForm onAdd={addHandler} />
             </div>
         </div>
     );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,25 +1,42 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
-export default function TodoForm() {
-    const [title, setTitle] = useState('');
+interface TodoFormProps {
+    onAdd(title: string): void;
+}
 
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value);
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+    // const [title, setTitle] = useState('');
+
+    const ref = useRef<HTMLInputElement>(null);
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value);
+    // };
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            // eslint-disable-next-line react/prop-types
+            props.onAdd(ref.current!.value);
+            ref.current!.value = '';
+            // console.log(title);
+            // setTitle('');
+        }
     };
 
     return (
         <div className="input-field mt2">
             <input
-                onChange={changeHandler}
-                value={title}
+                // onChange={changeHandler}
+                // value={title}
+                ref={ref}
                 type="text"
                 name="Some"
                 id="title"
                 placeholder="Введите название дела"
+                onKeyPress={keyPressHandler}
             />
             <label htmlFor="title" className="active">
                 Введите название дела
             </label>
         </div>
     );
-}
+};
